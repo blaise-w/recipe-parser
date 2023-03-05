804033f98ffb28d29131b49dce00b016159b3a26
blaise-w <blaisewebbeking2024@u.northwestern.edu>
2023-03-05 13:13:22 -0600

    commit

--- nlp2.py
+++ nlp2.py
@@ -5,7 +5,7 @@ from nltk import tokenize
 import re
 import requests
 
-COOKING_METHODS = ['boil', 'bake', 'fry', 'broil', 'steam', 'roast', 'airfry', 'cook', 'freeze', 'melt', 'saute', 'sauté', 'grill', 'melt', 'stirring', 'pour', 'dice', 'mince', 'heat', 'keep warm', 'reduce heat'] # two word instructions ??
+COOKING_METHODS = ['add', 'airfry', 'bake', 'boil', 'broil', 'chop', 'combine', 'cook', 'cool', 'cream', 'cube', 'cut', 'dice', 'drain', 'fry', 'freeze', 'garnish', 'grill', 'heat', 'knead', 'keep warm', 'marinate', 'melt', 'mince', 'mix', 'peel', 'poach', 'pour', 'preheat', 'reduce heat', 'roast', 'saute', 'sauté', 'season', 'simmer', 'slice', 'steam', 'stir', 'strain', 'toast', 'whisk']
 INGREDIENTS = ['tomatoes', 'pasta', 'oil', 'garlic', 'tomato paste', 'salt', 'pepper', 'basil', 'cheese', 'water']
 # edge case tomato versus tomato paste?? idk
 TOOLS = ['skillet', 'pan', 'pot', 'bowl', 'knife', 'oven']
@@ -221,9 +221,23 @@ def generate_recipe(url):
     Recipe.organizeInfo(r, text)
     return r
 
-def stepbot(rec):
-    #url = input('Enter the link to the recipe: ')
-    r = rec
+def generate_youtube(command):
+    command_words = command.split(' ')
+    link = "https://www.youtube.com/results?search_query="
+    for word in command_words:
+        link = link + "+" + str(word)
+    print("This link should help: " + link)
+
+def generate_google(command):
+    command_words = command.split(' ')
+    link = "https://www.google.com/search?q="
+    for word in command_words:
+        link = link + "+" + str(word)
+    print("This link should help: " + link)
+
+def bot2():
+    url = input('Enter the link to the recipe: ')
+    r = generate_recipe(url)
     #Recipe.printinfo(r)
     #print("hey \n")
     cooking_methods, ingredients, tools = r.parse_steps(r)
@@ -285,14 +299,20 @@ def bot():
             c = input()
             if c == 'y':
                 valid = False
-                stepbot(r)
+                stepparser(r)
 
         elif choice == '2':
             valid = False
-            stepbot(r)
+            stepparser(r)
         else:
             print('Hmm I do not understand what you want me to do')
 
+def stepparser(r):
+    steps = r.steps
+    for step in steps:
+        print(step)
+        print(get_methods(step))
+    
 def get_methods(text):
     verblist = []
     tokens = tokenize.word_tokenize(text.lower())
